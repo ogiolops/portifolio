e497e3aaa2e1db920c7c6d8a69bb9fada1a40220
Giovanni Lopes <giovanni_lopes@outlook.com>
2020-12-27 14:45:33 -0300

    fornece dados para api

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: "cZvWDTqTGKg",
+        title: "Masonry GRID - NOVA FEATURE do CSS 2020 | Code/Drops #67",
+        duration: "57min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "p34kaxW0WjU",
+        title: "CRUD Completo em menos de 12 minutos | Code/Drops #65",
+        duration: "12min",
+        price: "Free" 
+    },
+    {
+        id: "pbO3nnqyxlE",
+        title: "9 atitudes que nos fazem querer desistir (e como superar) | Code/Drops #63",
+        duration: "20min",
+        price: "Free" 
+    },
+    {
+        id: "1SPkgcrIf74",
+        title: "Será que é possível aprender JavaScript com Desafios? | Code/Drops #62",
+        duration: "14min",
+        price: "Free" ,
+        featured: true
+    },
+    {
+        id: "XlNLjG2c9uM",
+        title: "Você não vai mais precisar ler depois desse robô 😊 | Code/Drops #60",
+        duration: "11min",
+        price: "Free" 
+    },
+    {
+        id: "7pqp3IKyC0s",
+        title: "7 Tags HTML que Você Precisa Conhecer | Code/Drops #58",
+        duration: "19min",
+        price: "Free",
+        featured: true
+    }
+]
